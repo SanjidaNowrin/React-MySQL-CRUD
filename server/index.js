93ebef0581613bb02875fbfb1d7f6052e7397d69
unknown <nowrin.cse18@gmaile.com>
2022-02-10 10:00:17 +0600

    password-change

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   user: "root",
   host: "localhost",
-  password: "sanjida99",
+  password: "password",
   database: "employeesystem",
 });
 // post data
